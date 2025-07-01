7413920d0a42189467570b13f188ac0f6ded1e2c
baha bouali <baha.bouali2020@gmail.com>
2025-07-01 10:20:05 +0100

    corrected index.html, added middleware and extended requirements.txt

--- backend/src/mysite/main.py
+++ backend/src/mysite/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class TodoItem (BaseModel) :
     id: int
